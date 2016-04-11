cd82ec6efbcc2522b73d474f4aae30a554c88696
Naoya Iwamoto <iwanao731@gmail.com>
2016-04-11 11:25:20 +0900

    correct some bugs

--- src/ofxDeformationTransfer.cpp
+++ src/ofxDeformationTransfer.cpp
@@ -5,7 +5,7 @@ using namespace DT;
 
 ofxDeformationTransfer::ofxDeformationTransfer()
 {
-
+	Eigen::initParallel();
 }
 
 ofxDeformationTransfer::~ofxDeformationTransfer()
@@ -15,106 +15,153 @@ ofxDeformationTransfer::~ofxDeformationTransfer()
 
 void ofxDeformationTransfer::setReferenceModel(ofMesh *src_ref, ofMesh *trg_ref)
 {
-	setDtTransferData(src_ref, trans.source_ref);	/* source */
-	setDtTransferData(trg_ref, trans.target);		/* target */
+	setDtTransferData(src_ref, trans.sourceA);	/* source */
+	setDtTransferData(trg_ref, trans.targetA);	/* target */
 
 	/* make Sparse Matrix A */
-	trans.A.resize(3*trans.target.n_triangle, trans.target.n_vertex);
-
-	for (int j = 0; j < trans.target.n_triangle; j++) {
-		int i1 = trans.target.triangle[j].indices[0];
-		int i2 = trans.target.triangle[j].indices[1];
-		int i3 = trans.target.triangle[j].indices[2];
-		ofVec3f e0 = trans.target.vertex[i2] - trans.target.vertex[i1];
-		ofVec3f e1 = trans.target.vertex[i3] - trans.target.vertex[i1];
-
-		/* construct Wj */
-		MatrixXd W(3, 2);
-		W.col(0) = Vector3d(e0.x, e0.y, e0.z);
-		W.col(1) = Vector3d(e1.x, e1.y, e1.z);
-
-		/* QR Decomposition */
-		MatrixXd Q(3, 2); MatrixXd R(2, 2); Q.setZero(); R.setZero();
-		QRFactorize(W, Q, R);
-		MatrixXd invRQT = R.inverse() * Q.transpose();
-
-		/* set Sparse Matrix A */
-
-		/* i1 */
-		trans.A.coeffRef(3*j+0, i1) = -invRQT(0, 0) - invRQT(1, 0);
-		trans.A.coeffRef(3*j+1, i1) = -invRQT(0, 1) - invRQT(1, 1);
-		trans.A.coeffRef(3*j+2, i1) = -invRQT(0, 2) - invRQT(1, 2);
-
-		/* i2 */
-		trans.A.coeffRef(3*j+0, i2) = invRQT(0, 0);
-		trans.A.coeffRef(3*j+1, i2) = invRQT(0, 1);
-		trans.A.coeffRef(3*j+2, i2) = invRQT(0, 2);
-
-		/* i3 */
-		trans.A.coeffRef(3*j+0, i3) = invRQT(1, 0);
-		trans.A.coeffRef(3*j+1, i3) = invRQT(1, 1);
-		trans.A.coeffRef(3*j+2, i3) = invRQT(1, 2);
+	trans.A.resize(3*trans.targetA.n_triangle, trans.targetA.n_vertex);
+	for (int j = 0; j < trans.targetA.n_triangle; j++) {
+		setMatrixA(j, vMat);
 	}
+
+	trans.A.setFromTriplets(vMat.begin(), vMat.end());
 	trans.At = trans.A.transpose();
 	trans.AtA = trans.At * trans.A;
 	solver.compute(trans.AtA);
+	if (solver.info() != Eigen::Success){
+		/* decomposition failed	*/
+		cout << solver.lastErrorMessage() << endl;
+	}
 }
 
-void ofxDeformationTransfer::transfer2TargetModel(ofMesh *deformed_src, ofMesh *deformed_trg)
+void ofxDeformationTransfer::setMatrixA(const int triIdx, std::vector<T> &v)
 {
-	MatrixXd F(3 * trans.source_ref.n_triangle, 3);
+	int i1 = trans.targetA.triangle[triIdx].indices[0];
+	int i2 = trans.targetA.triangle[triIdx].indices[1];
+	int i3 = trans.targetA.triangle[triIdx].indices[2];
+	ofVec3f e0 = trans.targetA.vertex[i2] - trans.targetA.vertex[i1];
+	ofVec3f e1 = trans.targetA.vertex[i3] - trans.targetA.vertex[i1];
+
+	/* construct Va */
+	MatrixXd Va(3, 2);
+	setMatrixCol(Va, e0, 0);
+	setMatrixCol(Va, e1, 1);
+
+	/* QR Decomposition */
+	MatrixXd Q(3, 2); MatrixXd R(2, 2); R.setZero();
+	QRFactorize(Va, Q, R);
+	MatrixXd invRQT = R.inverse() * Q.transpose();
+
+	/* set Sparse Matrix A */
+
+	/* i1 */
+	v.push_back(T(3 * triIdx + 0, i1, -invRQT(0, 0) - invRQT(1, 0)));
+	v.push_back(T(3 * triIdx + 1, i1, -invRQT(0, 1) - invRQT(1, 1)));
+	v.push_back(T(3 * triIdx + 2, i1, -invRQT(0, 2) - invRQT(1, 2)));
+
+	/* i2 */
+	v.push_back(T(3 * triIdx + 0, i2,  invRQT(0, 0)));
+	v.push_back(T(3 * triIdx + 1, i2,  invRQT(0, 1)));
+	v.push_back(T(3 * triIdx + 2, i2,  invRQT(0, 2)));
+
+	/* i3 */
+	v.push_back(T(3 * triIdx + 0, i3,  invRQT(1, 0)));
+	v.push_back(T(3 * triIdx + 1, i3,  invRQT(1, 1)));
+	v.push_back(T(3 * triIdx + 2, i3,  invRQT(1, 2)));
+}
+
+void ofxDeformationTransfer::transfer2TargetModel(ofMesh *sourceB, ofMesh *targetB)
+{
+	MatrixXd F(3 * trans.sourceA.n_triangle, 3);
+	F.setZero();
 
 	/* load deformed source model */
-	MeshModel source_def;
-	setDtTransferData(deformed_src, source_def);
+	setDtTransferData(sourceB, trans.sourceB);
 
 	/* set Matrix F */
-	for (int j = 0; j < trans.source_ref.n_triangle; j++) {
+#if 1
+	/* parallel computing */
+	std::vector<std::thread> workers;
+	std::atomic<int> i(0);
 
-		/* source reference */
-		int i1 = trans.source_ref.triangle[j].indices[0];
-		int i2 = trans.source_ref.triangle[j].indices[1];
-		int i3 = trans.source_ref.triangle[j].indices[2];
-		ofVec3f e0 = trans.source_ref.vertex[i2] - trans.source_ref.vertex[i1];
-		ofVec3f e1 = trans.source_ref.vertex[i3] - trans.source_ref.vertex[i1];
+	int numThreads = std::thread::hardware_concurrency();
+	numThreads = (numThreads < 1) ? 1 : numThreads;
 
-		/* construct V */
-		MatrixXd V(3, 2);
-		V.col(0) = Vector3d(e0.x, e0.y, e0.z);
-		V.col(1) = Vector3d(e1.x, e1.y, e1.z);
+	for (auto t = 0; t < numThreads; t++) {
+		workers.push_back(std::thread([&, t]() {
+			int index = 0;
 
-		MatrixXd Q(3, 2); MatrixXd R(2, 2); Q.setZero(); R.setZero();
-		QRFactorize(V, Q, R);
-		MatrixXd invRQT = R.inverse() * Q.transpose();
-
-		/* source deform */
-		i1 = source_def.triangle[j].indices[0];
-		i2 = source_def.triangle[j].indices[1];
-		i3 = source_def.triangle[j].indices[2];
-		e0 = source_def.vertex[i2] - source_def.vertex[i1];
-		e1 = source_def.vertex[i3] - source_def.vertex[i1];
-
-		/* construct Vd */
-		MatrixXd Vd(3, 2);
-		Vd.col(0) = Vector3d(e0.x, e0.y, e0.z);
-		Vd.col(1) = Vector3d(e1.x, e1.y, e1.z);
-
-		MatrixXd S = Vd * invRQT;
-		SetMatrixBlock(F, S, j*3, 0);
+			while ((index = i++) < trans.targetA.n_triangle) {
+				setMatrixF(i - 1, F);	/* processing */
+			}
+		}));
 	}
 
-	/* apply the deformation */
+	for (auto &t : workers) {
+		t.join();
+	}
+#else
+	for (int j = 0; j < trans.sourceA.n_triangle; j++) {
+		setMatrixF(j, F);
+	}
+#endif
+
+	/* solve deformation transfer */
 	MatrixXd UtS = trans.At * F;
 	trans.x = solver.solve(UtS);
 
-	for (int i = 0; i < trans.target.n_vertex; i++) {
-		deformed_trg->setVertex(i, ofVec3f(trans.x(i, 0), trans.x(i, 1), trans.x(i, 2)));
+	if (solver.info() != Eigen::Success){
+		/* solving failed */
+		cout << solver.lastErrorMessage() << endl;
+	}
+
+	/* apply the deformation */
+	for (int i = 0; i < trans.targetA.n_vertex; i++) {
+		targetB->setVertex(i, ofVec3f(trans.x(i, 0), trans.x(i, 1), trans.x(i, 2)));
 	}
 }
 
-bool ofxDeformationTransfer::loadOBJModel(string filename, MeshModel model)
+void ofxDeformationTransfer::setMatrixF(const int triIdx, MatrixXd &F)
 {
-	return true;
+	/* deformation gradient for source model */
+	MatrixXd Va(3, 3);
+	MatrixXd Vb(3, 3);
+	setTriEdgeMatrix(Va, trans.sourceA, triIdx);
+	setTriEdgeMatrix(Vb, trans.sourceB, triIdx);
+
+	/* RQ decomposition */
+	MatrixXd Q(3, 3); MatrixXd R(3, 3); R.setZero();
+	QRFactorize(Va, Q, R);
+	MatrixXd invRQT = R.inverse() * Q.transpose();
+	MatrixXd Sa = Vb * invRQT;
+	Sa.transposeInPlace();
+	setMatrixBlock(F, Sa, triIdx * 3, 0);
+}
+
+void ofxDeformationTransfer::setMatrixCol(MatrixXd &m, ofVec3f v, int iCol)
+{
+	m(0, iCol) = v.x;
+	m(1, iCol) = v.y;
+	m(2, iCol) = v.z;
+}
+
+void ofxDeformationTransfer::setTriEdgeMatrix(MatrixXd &Va, MeshModel &model, int iTri)
+{
+	int i1 = model.triangle[iTri].indices[0];
+	int i2 = model.triangle[iTri].indices[1];
+	int i3 = model.triangle[iTri].indices[2];
+
+	ofVec3f v1 = model.vertex[i1];
+	ofVec3f v2 = model.vertex[i2];
+	ofVec3f v3 = model.vertex[i3];
+
+	ofVec3f e1 = v2 - v1;
+	ofVec3f e2 = v3 - v1;
+	ofVec3f e3 = calcNormal(v1, v2, v3);
+
+	setMatrixCol(Va, e1, 0);
+	setMatrixCol(Va, e2, 1);
+	setMatrixCol(Va, e3, 2);
 }
 
 void ofxDeformationTransfer::setDtTransferData(ofMesh *mesh, MeshModel &model)
@@ -172,10 +219,10 @@ void ofxDeformationTransfer::QRFactorize(const MatrixXd &a, MatrixXd &q, MatrixX
 
 ofVec3f ofxDeformationTransfer::calcNormal(ofVec3f v1, ofVec3f v2, ofVec3f v3)
 {
-	return v1 + (v2 - v1).crossed(v3 - v1) / sqrt(((v2 - v1).crossed(v3 - v1)).length());
+	return v1 + ((v2 - v1).crossed(v3 - v1)) / sqrt(((v2 - v1).crossed(v3 - v1)).length());
 }
 
-void ofxDeformationTransfer::SetMatrixBlock(MatrixXd &mBig, MatrixXd &mSmall, int iRow, int iCol)
+void ofxDeformationTransfer::setMatrixBlock(MatrixXd &mBig, MatrixXd &mSmall, int iRow, int iCol)
 {
 	int r, c, rmax = mSmall.rows(), cmax = mSmall.cols();
 	for (r = 0; r<rmax; r++)
